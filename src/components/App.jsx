9869053c6d4ccc8f48ae44e6a32d1c677f30df1e
Sldvlld <sladvlad@gmail.com>
2023-05-26 13:16:38 +0300

    yes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Contacts } from 'Pages/Contacts/Contacts';
 import Home from '../Pages/Home/Home';
+
 import Login from '../Pages/Login/Login';
 import Register from 'Pages/Register/Register';
 import Layout from '../components/AppBar/Layout';
@@ -9,7 +10,7 @@ export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} path="/home" />
+        <Route index element={<Home />} path="/goit-react-hw-08-phonebook" />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contacts" element={<Contacts />} />
