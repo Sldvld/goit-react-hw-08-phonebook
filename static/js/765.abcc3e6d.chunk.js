3d9224ea13b10aa258a9c1f9a5a4829a6d34433a
Sldvld <113293839+Sldvld@users.noreply.github.com>
2023-05-28 19:44:18 +0000

    Deploying to gh-pages from @ Sldvld/goit-react-hw-08-phonebook@e041f8cd07a4152021628a31a160fd67fcf24519 🚀

---
+++ static/js/765.abcc3e6d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[765],{765:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=765.abcc3e6d.chunk.js.map
