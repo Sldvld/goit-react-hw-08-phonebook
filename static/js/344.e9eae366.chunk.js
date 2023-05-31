28c2e73772b38cff4bd7f2ef2e52756b2429a457
Sldvld <113293839+Sldvld@users.noreply.github.com>
2023-05-31 10:20:35 +0000

    Deploying to gh-pages from @ Sldvld/goit-react-hw-08-phonebook@e86b6ecf7d5eb78193a254d33b3ff7dbfb4d8a74 🚀

---
+++ static/js/344.e9eae366.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[344],{344:function(e,o,t){t.r(o),t.d(o,{default:function(){return a}});var r="Home_homeLabel__KoXzL",n=t(184);function a(){return(0,n.jsx)("h1",{className:r,children:"Welcome to your personal phonebook!"})}}}]);
+//# sourceMappingURL=344.e9eae366.chunk.js.map
